e813cf2aa3b9551a07852a2a929be703d0c38b03
mechlovin <57231893+mechlovin@users.noreply.github.com>
2022-12-28 16:54:30 +0700

    Revert "Merge branch 'master' of https://github.com/qmk/qmk_firmware"
    This reverts commit 4d6c2ab08174fee52d8f705524c114b9249db432.

--- keyboards/mechlovin/infinity875/rules.mk
+++ keyboards/mechlovin/infinity875/rules.mk
@@ -1,9 +1,3 @@
-# MCU name
-MCU = atmega32u4
-
-# Bootloader selection
-BOOTLOADER = atmel-dfu
-
 # Build Options
 #   change yes to no to disable
 #
@@ -16,8 +10,7 @@ NKRO_ENABLE = no            # Enable N-Key Rollover
 BACKLIGHT_ENABLE = no       # Enable keyboard backlight functionality
 RGBLIGHT_ENABLE = no        # Enable keyboard RGB underglow
 AUDIO_ENABLE = no           # Audio output
-CUSTOM_MATRIX = lite
 RGB_MATRIX_ENABLE = yes
 RGB_MATRIX_DRIVER = WS2812
 
-SRC += matrix.c
+DEFAULT_FOLDER = mechlovin/infinity875/rev1
